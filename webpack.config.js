28923c199fc2cab104132501bfbf0e6725fe038f
sam hinton <sam.hinton88@gmail.com>
2018-06-08 17:57:42 +0100

    added actions and reducers dirs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react', 'stage-1']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
   }
 }
