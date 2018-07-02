9d0ff211bb352e628708c5c5432a21cd779d7f67
sam hinton <sam.hinton88@gmail.com>
2018-07-02 21:22:15 +0100

    change root url in routes

--- src/actions/action_creators/blueprints.js
+++ src/actions/action_creators/blueprints.js
@@ -1,15 +1,19 @@
 import { ADD_BLUEPRINT, EDIT_BLUEPRINT, SET_ACTIVE_BLUEPRINT, ADD_RESOURCE_TO_BLUEPRINT, GET_BLUEPRINTS, DELETE_RESOURCE } from '../types';
 import axios from 'axios'
 
+const ROOT_URL = process.env.NODE_ENV === 'production'
+  ? 'https://dashboard.heroku.com/apps/express-blueprint'
+  : 'http://localhost:5000'
+
 export const addBlueprint = (id) => async dispatch => {
     const payload = {blueprintName: 'click to edit name...', resources: []};
-    const res = await axios.post(`http://localhost:5000/api/users/${id}/blueprints`, payload);
+    const res = await axios.post(`${ROOT_URL}/api/users/${id}/blueprints`, payload);
 
     dispatch({ type: ADD_BLUEPRINT, payload: res.data });
 }
 
 export const getBlueprints =  (userId) => async dispatch => {
-  const res = await axios.get(`http://localhost:5000/api/users/${userId}/blueprints`);
+  const res = await axios.get(`${ROOT_URL}/api/users/${userId}/blueprints`);
 
   console.log("response data",res.data)
   const processedBlueprints = res.data.map((bp) => {
@@ -34,7 +38,7 @@ export const addResourceToBlueprint = (resource, blueprintId, userId) => async d
   const resourceAsString = JSON.stringify(resource);
 
   const res = await axios
-      .post(`http://localhost:5000/api/users/${userId}/blueprints/${blueprintId}/resources`,
+      .post(`${ROOT_URL}/api/users/${userId}/blueprints/${blueprintId}/resources`,
         {resourceAsString, refs})
 
   dispatch({ type: ADD_RESOURCE_TO_BLUEPRINT, payload: res.data, blueprintId })
@@ -42,7 +46,7 @@ export const addResourceToBlueprint = (resource, blueprintId, userId) => async d
 
 export const deleteResourceFromBlueprint = (userId, blueprintId, resourceName) => async dispatch => {
   console.log('deleteResourceFromBlueprint action creator fired with params', userId, blueprintId, resourceName);
-  const res = await axios.delete(`http://localhost:5000/api/users/${userId}/blueprints/${blueprintId}/resources/${resourceName}`);
+  const res = await axios.delete(`${ROOT_URL}/api/users/${userId}/blueprints/${blueprintId}/resources/${resourceName}`);
 
 
   dispatch({ type: DELETE_RESOURCE, blueprintId, resourceName });
