43eebe33adffb6a61690e849315012d4b71e5382
sam hinton <sam.hinton88@gmail.com>
2018-07-02 22:45:46 +0100

    put back curly braces

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const morgan = require('morgan');
 
-const mongoUri = require('./config/keys');
+const {mongoUri} = require('./config/keys');
 
 mongoose.Promise = global.Promise;
 console.log('this should be the mongo uri', mongoUri)
